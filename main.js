274e948e49dd7e143d9d928a378f03d585a73593
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-02-08 13:11:55 +0400

    final

--- main.js
+++ main.js
@@ -1,35 +1,41 @@
-
-fetch('https://hotelbooking.stepprojects.ge/api/Hotels/GetAll', {
+fetch('https://hotelbooking.stepprojects.ge/api/Rooms/GetAll', {
    method: 'GET'
 })
 .then((response) => response.json())
 .then((data) => {
    const roomsContainer = document.getElementById("rooms");
-
-   data.forEach((hotel) => {
+   const totalRooms = data.length;
+   data.forEach((room) => {
        const card = document.createElement("div");
-       card.className = "hotelrooms";
+       card.className = "roomcard";
 
-       const image = hotel.featuredImage;
-       
-       const prices = hotel.rooms.map(room => room.pricePerNight);
-       const minPrice = Math.min(...prices);
-       const maxPrice = Math.max(...prices);
+       const image = room.images && room.images.length > 0 ? room.images[0].source : "";
 
        card.innerHTML = `
-           <img src="${image}" alt="${hotel.name}" style="width: 100%; height: 200px; object-fit: cover;">
-           <h2>${hotel.name}</h2>
-           <p class="price"><strong>ფასი: ${minPrice} - ${maxPrice} $</strong></p>
-           <p><strong>მისამართი:</strong> ${hotel.address}</p>
-           <p><strong>ქალაქი:</strong> ${hotel.city}</p>
-           <p><strong>ოთახების რაოდენობა:</strong> ${hotel.rooms.length}</p>
+           <img src="${image}" alt="${room.name}" style="width: 100%; height: 200px; object-fit: cover;">
+           <h2>${room.name}</h2>
+           <p><strong>ფასი:</strong> ${room.pricePerNight} $</p>
+           <p><strong>სტუმრების მაქსიმუმი:</strong> ${room.maximumGuests}</p>
+           <p><strong>ხელმისაწვდომია?:</strong> ${room.available ? "დიახ" : "არა"}</p>
+           <p><strong>Room ID:</strong> ${room.id}</p>
        `;
+
        roomsContainer.appendChild(card);
    });
+
+   const countInfo = document.createElement("p");
+   countInfo.innerHTML = `<strong>ოთახების რაოდენობა: ${totalRooms}</strong>`;
+   roomsContainer.appendChild(countInfo);
 })
 .catch((error) => {
-   console.error("მონაცემების წამოღებისას შეცდომა:", error);
+   console.error("შეცდომა:", error);
 });
+
+
+
+
+// თავიდან გამოვიტანე სასტუმროები ისე როგორც თქვენ გააკეთედ მაგრამ ფასი ვერ გამომქომნდა
+// (შემდეგ მივხვდი რო სასტუმროს კი არა ოთახს ექნება ფასი და ახლიდან დავიწყე ეს კოდი კი დამრჩა როგორც ბონუს დავალება)
 
 // fetch('https://hotelbooking.stepprojects.ge/api/Hotels/GetAll')
 // .then((response) => response.json())
